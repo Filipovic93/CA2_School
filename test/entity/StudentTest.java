af69d9b318920192d399a851917a7df788e32d4b
MessiKrkic <MessiKrkic@gmail.com>
2014-09-30 13:22:55 +0200

    Damjan made two tests (isolation tests)

---
+++ test/entity/StudentTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entity;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Filipovic
+ */
+public class StudentTest {
+    
+    private Student s;
+    
+    public StudentTest() {
+    }
+    
+    @Before
+    public void setUp() {
+          s = new Student("3. semester");
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetSemester() {
+     String sem = s.getSemester();
+        assertEquals(sem, "3. semester");
+    }
+
+    @Test
+    public void testSetSemester() {
+      String sem = ("3. semester");
+        s.setSemester(sem);
+        assertEquals(sem, s.getSemester(), sem);
+    }
+    
+}
---
+++ test/entity/TeacherTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entity;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Filipovic
+ */
+public class TeacherTest {
+    
+    private Teacher t;
+    
+    
+    public TeacherTest() {
+    }
+    
+    @Before
+    public void setUp() {
+            t = new Teacher ("Philosophy"); 
+    }
+    
+    @After
+    public void tearDown() {
+  
+    }
+
+    @Test
+    public void testGetDegree() {
+       String deg = t.getDegree();
+        assertEquals(deg, "Philosophy");
+    }
+
+    @Test
+    public void testSetDegree() {
+      String deg = ("Philosophy");
+      t.setDegree(deg);
+        assertEquals(deg, "Philosophy");
+    }
+    
+}
